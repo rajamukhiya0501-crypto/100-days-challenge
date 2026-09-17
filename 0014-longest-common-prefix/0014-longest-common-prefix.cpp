1b3dcd23098b529efe47d5c4be23489188db0f8e
RAJA <rajamukhiya0501@gmail.com>
2026-09-17 10:20:18 +0530

    Time: 0 ms (100%), Space: 11.9 MB (59.52%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+          string first = strs[0];
+        for(int i=0; i<first.length();i++){
+            char ch =first[i];
+            for(int j=1;j<strs.size();j++){
+                if(i>=strs[j].length() || strs[j][i] != ch){
+                    return first.substr(0,i);
+                }
+            }
+        }
+        return first;
+    }
+          
+      
+
+
+    
+};
